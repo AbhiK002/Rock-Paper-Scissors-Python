35d94f29b2a4daabfe6f5bfd88521282ac3519f1
AbhineetK <68178267+AbhiK002@users.noreply.github.com>
2021-01-24 20:00:48 +0530

    Improved the code (added if __name__ ==......)

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,31 +5,31 @@ import time
 
 
 def ask_username():
+    # Asks and Stores your username
+    # it should contain only alphabets and numbers
     while True:
-        username = input("Enter Your Username: ")  # Asks for a Name from the user
-        if username.isalnum():  # If username has only alphabets and numbers
+        username = input("Enter Your Username: ")
+        if username.isalnum():
             print()
-            print(f"Welcome to Rock Paper Scissors, {username}")
+            print('\n' + f"Welcome to Rock Paper Scissors, {username}" + '\n')
             print()
-            # program has now stored your username
             break
-        elif username == "":  # If the username is blank, prompt to type it again
-            print("Please enter a username")
-            print()
+        elif username == "":  
+            print("Please enter a username" + '\n')
         else:
-            print("Please ensure your username only contains alphabets and numbers")
-            print()
-    # since this is a function, it needs to return the username variable
-    # for other functions to use
+            print("Please ensure your username only contains alphabets and numbers" + '\n')
+    # Returns the username for future use
     return username
 
 
 def clear():
-    # Clears the Console Output Screen completely
+    # Clears the Output Screen in Console to make the game output neat
+    # Separates Series of Matches to avoid confusion
     os.system('cls')
 
 
-def rules():  # Prints the rules to play the game
+def rules():
+    # Prints all the rules of the game
     print(("=" * 42))
     print("~ RULES ~".center(42))
     print("""Press the following keys to play the game:
@@ -41,8 +41,8 @@ def rules():  # Prints the rules to play the game
     print(("=" * 42))
 
 
-def difficulty():  # function that will decide the difficulty
-    game_difficulty = None  # Difficulty not chosen yet
+def difficulty():
+    game_difficulty = "normal"  # Difficulty by default is random (normal)
     print("""Which Difficulty would you like to play on? (Press the keys B, N, I or ESC)
     B   - Baby
     N   - Normal
@@ -50,47 +50,49 @@ def difficulty():  # function that will decide the difficulty
     
     ESC - Quit Game
     """)
-    # While loop to check for the input of the particular keyboard keys
     while True:
-        # Different keys will set the difficulty for the game, defined by a string (e.g. "easy")
+        # Keeps checking for key presses until valid keys pressed
         if keyboard.is_pressed('n'):
-            game_difficulty = "normal"
-            # Computer will make a random choice thus giving 3 possibilities
-            # Either a draw, win or loss
+            # by default difficulty is always normal
+            # normal difficulty has equally likely win, loss or draw for the user
             break
         elif keyboard.is_pressed('b'):
             game_difficulty = 'baby'
-            # Computer will lose everytime
+            # player always wins in this difficulty
             break
         elif keyboard.is_pressed('i'):
             game_difficulty = "impossible"
-            # Computer will win everytime
+            # player always loses in this difficulty
             break
-        elif keyboard.is_pressed('esc'):  # ESC key ends the program immediately
+        elif keyboard.is_pressed('esc'):
+            # program quits instantly if esc pressed
             quit()
-    # returns the difficulty string for other functions to use
+    # returns difficulty for future use
     return game_difficulty
 
 
-def game():  # Starts a new game
-    # New Game is basically a new series of matches
+def game():
+    # function which contains the main game
+    # including series, difficulty and matches details
     print(game_difficulty.upper().center(33, "-"))
-    match_count = 0  # represents the number of matches that have been played
-    computer_wins = 0  # number of wins of the computer
-    player_wins = 0  # number of wins of the user
+    # this parts marks the beginning of a new series
+    # number of wins and matches played are set to zero
+    match_count = 0  
+    computer_wins = 0  
+    player_wins = 0 
     while True:
-        print()
-        print(f"Match - {match_count + 1}".center(33))  # Prints the current match number being played
-        print()
-        print(f"{username} : ", end='', flush=True)  # Shows the user what they've chosen (e.g. User : Rock)
-        player_choice = None  # Player yet to make a choice
-        computer_choice = None  # Computer yet to make a choice
-        game_restarted = False  # New game is not to be started by default (while loop will run repeatedly by default)
+        # this loop marks every match in a series
+        print('\n' + f"Match - {match_count + 1}".center(33) + '\n')
+        print(f"{username} : ", end='', flush=True)
+        player_choice = None  
+        computer_choice = None 
+        game_restarted = False  # by default series is supposed to continue
         while True:
-            if keyboard.is_pressed('r'):  # To choose a rock press R, similarly for paper and scissor
+            # Loop to check for user input to select item
+            if keyboard.is_pressed('r'):  
                 time.sleep(0.05)
                 print("Rock")
-                player_choice = 'Rock'  # Player choice is updated to string "Rock", similarly for the others
+                player_choice = 'Rock' 
                 break
             elif keyboard.is_pressed('p'):
                 time.sleep(0.05)
@@ -102,178 +104,109 @@ def game():  # Starts a new game
                 print("Scissors")
                 player_choice = 'Scissors'
                 break
-            elif keyboard.is_pressed('space'):  # To start a new game, press space-bar
-                print('\r', end='')  # Overwrites the "User : " printed on the console
-                game_restarted = True  # The player has chosen to start a new game by pressing space-bar
+            elif keyboard.is_pressed('space'):
+                # To end the current series and start a new one with a new difficulty
+                print('\r', end='')  
+                game_restarted = True 
                 break
-            elif keyboard.is_pressed('esc'):  # Program ends after 0.5 seconds when ESC pressed
+            elif keyboard.is_pressed('esc'): 
                 time.sleep(0.5)
                 exit()
-        if not game_restarted:  # If the space-bar hasn't been pressed, by default game_restarted is False
+        if not game_restarted:  # if the user doesn't press space bar, series will continue
             match_count += 1
-            # player made a choice and the computer will do so too
-            # Hence the first match will be completed soon. Program updates match_count already
-
             items = ['Rock', 'Paper', 'Scissors']
-            # List from which computer will choose an item
-
             winner_criteria = {'Rock': 'Scissors', 'Scissors': 'Paper', 'Paper': 'Rock'}
-            # Stores the information about which item beats which item
 
-            if game_difficulty == 'normal':  # Random Choice, 3 Equally likely Possibilities
+            # This part decides the choice of computer depending upon difficulty
+            if game_difficulty == 'normal': 
                 computer_choice = random.choice(items)
-                # Computer makes a random choice from the Items list
-
-            elif game_difficulty == 'baby':  # Computer loses everytime
+            elif game_difficulty == 'baby': 
                 computer_choice = winner_criteria[player_choice]
-                # The computer simply chooses an item that will make the user win
-                # The user has already made a choice so the computer knows how to lose
-
-            elif game_difficulty == 'impossible':  # Computer wins everytime
+            elif game_difficulty == 'impossible':
+                # Makes sure player always loses
                 for winning_item, losing_item in winner_criteria.items():
-                    # Checks the criteria (dictionary) to select the item that will beat the user
                     if losing_item == player_choice:
-                        computer_choice = winning_item  # Chooses the item that beats the user
-            print(f"Computer : {computer_choice}")  # Prints computer's choice
-            print()
+                        computer_choice = winning_item  
+            print(f"Computer : {computer_choice}" + '\n')
 
+            # This part decides the winner/draw
             winner_output = None
-            # No one has won yet, the program has to yet calculate that
-
-            # This part of code decides the winner
-            if player_choice == computer_choice:  # If the items chosen by both are same
-                # Only for Normal Possibilities
-
-                winner_output = "~ MATCH TIED ~"  # Sets the result for the current match
-
-            elif winner_criteria[player_choice] == computer_choice:  # If user's item beats computer's item
-                # Only for Normal and Baby Difficulties
-
-                winner_output = f"~ {username.upper()} WINS ~"  # Sets user as the winner for current match
-                player_wins += 1  # User's wins are updated since user won
-
-            else:  # Computer's item beats User's item
-                # Only for Normal and Impossible Difficulties
-
-                winner_output = "~ COMPUTER WINS ~"  # Sets Computer as the winner for current match
-                computer_wins += 1  # Computer's wins are updated since it won
-
-            print(winner_output.center(33))  # Prints the winner of current match
-
-            # This prints the current score of the series
+            if player_choice == computer_choice: 
+                winner_output = "~ MATCH TIED ~"  
+            elif winner_criteria[player_choice] == computer_choice: 
+                winner_output = f"~ {username.upper()} WINS ~"  
+                player_wins += 1 
+            else: 
+                winner_output = "~ COMPUTER WINS ~"  
+                computer_wins += 1 
+            print(winner_output.center(33))
+            # Prints the current score after every match
             print(f"{username}'s Wins: {str(player_wins)}".ljust(16) + f"Computer's Wins: {str(computer_wins)}".rjust(20))
-
-            print()
-            print(" =" * 16)  # Separates every match output to avoid confusion
-            print()
+            print('\n' + " =" * 16 + '\n')
             time.sleep(0.5)
-        else:  # If the player chose to restart the game (basically reset the difficulty)
-            break  # Simply breaks out of the loop which asks for User's input
-
-    # At some point, the user has to start a new game, therefore when the user does so
-    # the Function returns the number of wins of both sides as well as the number of matches played
-    # for other functions to use
+        else:
+            print("*CANCELLED*".center(33) + '\n')  # to avoid confusion
+            time.sleep(0.8)
+            # if the user ended the series by pressing space bar
+            break
+    # the three variables returned for future use if user decides to end series
     return player_wins, computer_wins, match_count
 
 
-def series_winner():  # This Function prints the summary of the current series before restarting the game
-    # It uses the values returned by the previous function i.e. game()
-
-    print("*CANCELLED*".center(33))
-    # Since the match number is still displayed on the console
-    # and the player chose to stop the game during that match, this prints CANCELLED below it
-    # this clarifies (only for the user) the current match isn't to be counted as "played"
-
-    print()
+def series_winner():
+    # Before starting next series, this function prints the winner and other details about the current series
+    # Basically a summary (number of matches, overall winner, etc) of the current series are printed
+    diff_output = f"Difficulty: {game_difficulty.title()}"  
     time.sleep(0.8)
-    diff_output = f"Difficulty: {game_difficulty.title()}"  # output that showing the difficulty of the current series
-    time.sleep(0.8)
-    winner = None  # Computer has to yet calculate the Winner of the SERIES (Not that match)
-    score = str(player_wins) + "-" + str(computer_wins)  # defines the default total score output (Player - Computer)
-    if player_wins > computer_wins:  # If user has more wins than computer
-        winner = f"{username}"  # winner is updated to user
-    elif player_wins < computer_wins:  # If computer has more wins than user
-        winner = "Computer"  # Winner updated to computer
-        score = str(computer_wins) + "-" + str(player_wins)  # score output redefined to (Computer - Player)
+    winner = None  # Overall Winner not decided yet
+    score = str(player_wins) + "-" + str(computer_wins) 
+    if player_wins > computer_wins: 
+        winner = f"{username}"  
+    elif player_wins < computer_wins:  
+        winner = "Computer" 
+        score = str(computer_wins) + "-" + str(player_wins)
     else:
-        winner = "DRAW"  # Winner is updated to "DRAW"
+        winner = "DRAW"
 
+    # The Series Details are printed
     series_details_output = f"Series {series_count} // Total Matches: {match_count}"
-    # Details about the number of current series and total matches played in that particular series
-
     series_winner_output = f"Winner: {winner} [{score}]"
-    # the winner and the score that is to be printed
-
-    print("=" * len(series_details_output))  # Final Summary of Series Separator
-    print(diff_output.center(len(series_details_output)))  # Prints the difficulty of the series
-    print(series_details_output)  # Prints the details about the series
-    print(series_winner_output.center(len(series_details_output)))  # Prints the winner and score variable
-    print("=" * len(series_details_output))  # Final Summary of Series Separator
-    print()
+    print("=" * len(series_details_output))  
+    print(diff_output.center(len(series_details_output)))  
+    print(series_details_output) 
+    print(series_winner_output.center(len(series_details_output)))
+    print("=" * len(series_details_output) + '\n')
     print("Please Start a New Game by Pressing SPACE BAR")
     print("Press ESCAPE key to quit game")
 
 
-def next_game_trigger():  # To start a new game
-    while True:  # While loop checks for input whether
+def next_game_trigger():
+    # Program won't abruptly start a new series after printing summary
+    # instead this function will wait for the user (to press spacebar or esc)
+    while True: 
         if keyboard.is_pressed('space'):
-            # Breaks this loop when space is pressed to restart the main loop (at the end)
             break
-        elif keyboard.is_pressed('esc'):  # Quits game after 2 seconds if ESC pressed
-            print()
-            print("Quitting Game....")
+        elif keyboard.is_pressed('esc'): 
+            print('\n'+"Quitting Game...."+'\n')
             time.sleep(2)
             quit()
 
 
-# MAIN PART OF THE CODE
-# Uses all the functions together to complete and run the game
-
-# Function asks for the user's name and then stores it in "username" variable used throughout the program
-username = ask_username()
-
-# Number of Series that have been started
-series_count = 0
-
-while True:  # Main Loop
-    # The current number of series is updated everytime the loop starts
-    series_count += 1
-
-    # Prints the rules on the screen
-    rules()
-
-    # Stores the difficulty chosen by user in the "game_difficulty" variable
-    game_difficulty = difficulty()
-
-    # Clears the console output once difficulty chosen
-    clear()
-
-    # Prints the rules again on the top since all the output was cleared
-    rules()
-
-    print()
-    print(f"SERIES - {series_count}".center(33))
-    # Prints the Current Series Number on Screen before running game() function
-    print()
-
-    
-    # The game() function is a while loop, so it will keep running unless SPACE-BAR is pressed
-    # It keeps printing the match details (points and winner of every match)
-    # When user presses SPACE-BAR, while loop inside the function breaks (The Current Series ends)
-    # Now the game() function will give three values
-    player_wins, computer_wins, match_count = game()
-    # It Stores the three values from the game() function as seen above
-
-    # This function prints the Series Summary and also decides the winner of the particular series
-    series_winner()
-    # The player can view the summary and see the stats of the series... the program will not move further
-
-    # it waits for the user
-    # to either exit the game or restart the game
-    # restarting the game basically means resetting difficulty and start a new series
-    # if the previous series was (Series x) then the new series will be (Series x + 1)
-    next_game_trigger()
-
-    # If the user chooses to restart, the output screen will be cleared
-    # this makes space for the next series
-    clear()
+if __name__ == '__main__':
+    # Main part that runs the whole game
+    username = ask_username()
+    series_count = 0
+    while True:
+        # Everytime this loop repeats (series ended)
+        # Series number increases by 1
+        series_count += 1
+        rules()
+        game_difficulty = difficulty()
+        print(game_difficulty)
+        clear()
+        rules()
+        print('\n' + f"SERIES - {series_count}".center(33) + '\n')
+        player_wins, computer_wins, match_count = game()
+        series_winner()
+        next_game_trigger()
+        clear()
